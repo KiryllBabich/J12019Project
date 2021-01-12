e7a1ef563105628027424947a455bad1d74e00c3
Kirill <333pontipilat333@mail.ru>
2021-01-12 18:59:44 +0300

    3commit

--- ExWork/src/main/java/FirstTestClass.java
+++ ExWork/src/main/java/FirstTestClass.java
@@ -4,6 +4,7 @@ public class FirstTestClass {
 	public static void main(String[] args) {
 		System.out.println("start commit");
 		System.out.println("again commit");
+		System.out.println("and again commit");
 	}
 
 }
